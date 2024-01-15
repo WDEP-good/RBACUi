7a9b7bd3b3bd24d1c8efabd7dbeab21db5e119cb
WDEP <13300444+wdep@user.noreply.gitee.com>
2024-01-15 20:45:59 +0800

    feat: addH1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <h1></h1>
+    <h1>qdqdqdqdqdqdq</h1>
+    <input type="text" name="" id="" />
+    <textarea name="" id="" cols="30" rows="10">
+      <h1>
+        11313
+      </h1>
+      <h4>eqdoqdj</h4>
+
+
+    </textarea>
   </div>
 </template>
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: yellow;
+}
+</style>
